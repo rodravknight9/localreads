11c215021334d5235ecc05569d8a5628b6b5a2e4
Rodrigo Alarcon Vargas <dealarcmarcel99@gmail.com>
2025-05-23 23:30:25 -0400

    Add project files.

---
+++ OpenLibraryBlazorApp/Services/IOpenLibraryService.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenLibraryBlazorApp.Services
+{
+    public interface IOpenLibraryService
+    {
+        public Task GetRandomBooks();
+
+        //https://openlibrary.org/search.json?q=the&limit=5&offset=1&fields=author_key,author_name,first_publish_year,key,language,title,isbn
+        //https://covers.openlibrary.org/b/isbn/1986537536.jpg
+    }
+}
---
+++ OpenLibraryBlazorApp/Services/OpenLibraryService.cs
@@ -0,0 +1,19 @@
+﻿
+namespace OpenLibraryBlazorApp.Services
+{
+    public class OpenLibraryService : IOpenLibraryService
+    {
+        private readonly HttpClient _httpClient;
+        public OpenLibraryService()
+        {
+            _httpClient = new HttpClient() 
+            {
+                BaseAddress = new Uri("https://openlibrary.org")
+            };
+        }
+        public Task GetRandomBooks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
