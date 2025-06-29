0466c1a9541a1ad3e47a7212e11d3dafa994dce0
rodravknight <dealarcmarcel99@gmail.com>
2025-06-28 20:31:42 -0400

    Fix: Table is re-rendered after a search component updates the Search state without invoking an infinite loop

--- LocalReads/State/SearchResults.cs
+++ LocalReads/State/SearchResults.cs
@@ -4,7 +4,19 @@ namespace LocalReads.State;
 
 public class SearchResults
 {
-    public List<BookDto> Books { get; set; } = new List<BookDto>();
+    private List<BookDto> _books;
+    public List<BookDto> Books 
+    { 
+        get => _books;
+        set
+        {
+            _books = value;
+            NotifySearchResultsChanged();
+        }
+    } 
+
+
+
     private string _termSearch;
     public string TermSearch { 
         get 
@@ -19,10 +31,14 @@ public class SearchResults
     }
     public event Action? OnChange;
 
+    public event Action? OnBooksSearchChanged;
     public void SetSearchResults(List<BookDto> books)
     {
+        //NotifySearchResultsChanged();
         Books = books;
         NotifyStateChanged();
     }
     private void NotifyStateChanged() => OnChange?.Invoke();
+    private void NotifySearchResultsChanged() => OnBooksSearchChanged?.Invoke();
+
 }
