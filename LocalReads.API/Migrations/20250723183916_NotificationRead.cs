afb2db5ee3020d4c111b15ebf4d426e614a3d5e4
rodravknight <dealarcmarcel99@gmail.com>
2025-07-24 22:27:35 -0400

    Added a new table to hold read notifications

---
+++ LocalReads.API/Migrations/20250723183916_NotificationRead.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocalReads.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class NotificationRead : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NotificationReads",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    LogActionId = table.Column<int>(type: "INTEGER", nullable: false),
+                    AlreadyRead = table.Column<bool>(type: "INTEGER", nullable: false),
+                    ReadOn = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotificationReads", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NotificationReads_LogActions_LogActionId",
+                        column: x => x.LogActionId,
+                        principalTable: "LogActions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_NotificationReads_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NotificationReads_LogActionId",
+                table: "NotificationReads",
+                column: "LogActionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NotificationReads_UserId",
+                table: "NotificationReads",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NotificationReads");
+        }
+    }
+}
